a3b5522904e6345661fc2668ca8a5b8a5ec9a82d
Tewodros-Berhane <tewodrosberhaneabraha@gmail.com>
2026-01-27 21:19:35 +0300

    feat(branding): update page title and favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,7 +32,8 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any">
+        <link rel="icon" href="/favicon.svg?v=port-101" type="image/svg+xml" sizes="any">
+        <link rel="shortcut icon" href="/favicon.svg?v=port-101" type="image/svg+xml">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
