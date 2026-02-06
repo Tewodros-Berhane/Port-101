79fc198eb07e6b321cdb930998b2c9f25929e218
Tewodros-Berhane <tewodrosberhaneabraha@gmail.com>
2026-02-06 14:20:53 +0300

    feat(ui): align app logo with homepage
    Use the same Port-101 icon across layouts.

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -1,13 +1,6 @@
+import { Boxes } from 'lucide-react';
 import type { SVGAttributes } from 'react';
 
 export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
-    return (
-        <svg {...props} viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
-            <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M12 6H22C29.1797 6 34 10.8203 34 18C34 25.1797 29.1797 30 22 30H16V36H12V6ZM16 10V26H22C26.9706 26 30 22.9706 30 18C30 13.0294 26.9706 10 22 10H16Z"
-            />
-        </svg>
-    );
+    return <Boxes {...props} />;
 }
