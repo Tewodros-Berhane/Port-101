4df3138b687de5d4be905d7995c65cc0527db8f9
Tewodros-Berhane <tewodrosberhaneabraha@gmail.com>
2026-04-07 00:43:00 +0300

    fix(mail): simplify branded header lockup

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -2,11 +2,8 @@
 <x-slot:header>
 <x-mail::header :url="config('app.url')">
 <div class="brand-lockup">
-    <span class="brand-mark">P101</span>
-    <div class="brand-copy">
-        <span class="brand-name">{{ config('app.name') }}</span>
-        <span class="brand-tagline">Operational ERP for finance, inventory, projects, and HR</span>
-    </div>
+    <span class="brand-name">{{ config('app.name') }}</span>
+    <span class="brand-tagline">Operational ERP for finance, inventory, projects, and HR</span>
 </div>
 </x-mail::header>
 </x-slot:header>
